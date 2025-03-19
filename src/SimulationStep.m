a1509e4cd0e56ffe69dfb19562c060ae4f67a37a
IestynE <idedwards15@gmail.com>
2025-03-19 13:20:51 +0000

    Suffering
    Pain and Suffering

--- src/SimulationStep.m
+++ src/SimulationStep.m
@@ -8,5 +8,29 @@ Fwall = WallCollision(x,box,ball) ;
 Fgrav = [0;-g] ;
 
 Forces = Fcollide + Fwall + Fgrav ;
+
 xnew = x + h*v + h^2*Forces ;
 vnew = (xnew - x)/h ;
+
+%{ 
+Jan's Method:
+xnew = x + h*v + h^2*Forces
+...  = x + h*(v + h*Forces)
+
+vnew = (xnew - x)/h
+...  = (x + h*v + h^2*Forces - x)/h
+...  = (h*v + h^2*Forces)/h
+...  = v + h*Forces (things we already know)
+
+Symplectic:
+vnew = v + h*g(tn,xn)
+xnew = x + h*f(tn,vn+1)
+
+
+dx/dt = f(t,v) and dv/dt = g(t,x) btw.
+Surely g(tn,xn) is Forces. Since it's the acceleration at our current step.
+f(tn,vn+1) HAS to just be vn+1.
+
+so vnew = v + h*Forces
+   xnew = x + h*vnew
+%}
--- src/boxColour.m
+++ src/boxColour.m
@@ -1,9 +1,13 @@
 function colours = boxColour(boxIndex,subBox) 
 
-% Laziest colour scheme of your life
-% First row scales colour based on x index, from 0 to 1.
-% Second along y index, from 0 to 1.
-% Third row is just 1's so that we get 1 1 1.
-colours = [1 - (boxIndex(1,:)-1)./(subBox.x-1) ; 1 - (boxIndex(2,:)-1)./(subBox.y-1) ; 1 + zeros(1,length(boxIndex))]' ;
+%{
+- First row scales colour based on x index, from 0 to 1.
+- Second along y index, from 0 to 1.
+- Third row is just 1's so that we get 1 1 1.
+When the wall shrinks, the subboxxes do too, so these colours are based on
+subbox arrangement/index, not position.
+So when the height halves, the colours are now determined by rectangles.
+%}
+colours = [1 - (boxIndex(1,:)-1)./(subBox.x-1) ; 1 - (boxIndex(2,:)-1)./(subBox.y-1) ; 1 + zeros(1,size(boxIndex,2))]' ;
 
 end
