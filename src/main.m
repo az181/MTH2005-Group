074113abb708cd7b44b68d190df7a8ca123c4b8b
Jk735 <146760194+Jk735@users.noreply.github.com>
2025-03-26 17:21:31 +0000

    Fixed doingTask2 variable refactoring error

--- src/main.m
+++ src/main.m
@@ -16,7 +16,7 @@ end
 %% Draw Graphs
 iWantGraphs = true ; 
 if iWantGraphs
-    drawGraphs(graphData, doingTask2, doingTask3)
+    drawGraphs(graphData, doingSD, doingTask3)
     t4PressureMinimisation(graphData, subBox, box)
 end
 
