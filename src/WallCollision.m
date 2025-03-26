a531bf7830f4da9e524b5fbb9b68ded047e686c7
Jk735 <146760194+Jk735@users.noreply.github.com>
2025-03-26 18:48:38 +0000

    Comments

--- src/WallCollision.m
+++ src/WallCollision.m
@@ -1,8 +1,8 @@
 function wallForce = WallCollision(x,box,ball)
-
+%forces on balls from each walls
 Fleft = max(0,ball.radius + box.left - x(1,:))    ;
 Fright = max(0,ball.radius - box.right + x(1,:))  ;
 Fbot = max(0,ball.radius + box.low - x(2,:))      ;
 Fup = max(0,ball.radius - box.up + x(2,:))        ;
 
-wallForce = ball.spring.*[Fleft - Fright;Fbot-Fup] ;
+wallForce = ball.spring.*[Fleft - Fright;Fbot-Fup] ;%total overall force on ball due to walls
--- src/temperatureByY.m
+++ src/temperatureByY.m
@@ -1,6 +1,6 @@
 function out = temperatureByY(vs, boxIndex, subBoxSize)
-    tempOfYBoxMask = @(i) ((vs(1,:)~=0 | vs(2,:)~=0) & ...
+    tempOfYBoxMask = @(i) ((vs(1,:)~=0 | vs(2,:)~=0) & ...%makes N length list to make binary mask
     (boxIndex(1,:)==subBoxSize.x | boxIndex(1,:)==1) & boxIndex(2,:)==i);
-    speeds = arrayfun(@(i) norm(vs(:,i)), 1:size(vs,2));
-    out = arrayfun(@(i) sum(speeds .* tempOfYBoxMask(i)), 1:subBoxSize.y)';
+    speeds = arrayfun(@(i) norm(vs(:,i)), 1:size(vs,2));   %finds speeds for all y values
+    out = arrayfun(@(i) sum(speeds .* tempOfYBoxMask(i)), 1:subBoxSize.y)';%applies to all 
 end
