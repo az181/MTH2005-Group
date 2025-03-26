3592002121e3b8cdafc7148bb5ba819965c287cf
Jk735 <146760194+Jk735@users.noreply.github.com>
2025-03-26 17:11:37 +0000

    Did the pressure by y graph

--- src/t4PressureMinimisation.m
+++ src/t4PressureMinimisation.m
@@ -1,15 +1,15 @@
 function t4PressureMinimisation(graphData, subBox, box)
 
-pressure = graphData.pressureByY;
+pressure = graphData.pressureByY';
 
 y = box.up/(2*subBox.y) : box.up/subBox.y : box.up ;
 
 params_guess = [1/pressure(1), (1/pressure(end) - 1/pressure(1))/box.up];
 
-f = @(params) sum(abx(pressure - 1./params(1) + params(2).y));
+f = @(params) sum(abs(pressure - 1./(params(1) + params(2).*(y))));
 
 params_min = fminsearch(f, params_guess);
-
+format long
 figure(80)
 bar(y,pressure)
 xlabel('y value')
@@ -18,11 +18,11 @@ ylabel('Average Pressure')
 figure(81)
 hold on
 bar(y,pressure)
-pressure_est = 1./(params_min(1) + params_min(2).*(y.^2)) ;
+pressure_est = 1./(params_min(1) + params_min(2).*(y)) ;
 plot(y,pressure_est,'r',linewidth=2)
 xlabel('y value')
 ylabel('Average Pressure')
-legend('Particle Pressure by y','1/a+by^2 approximation')
+legend('Particle Pressure by y','1/a+by approximation')
 hold off
 
 end
